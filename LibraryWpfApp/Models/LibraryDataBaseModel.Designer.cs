dcfa6bd2a49171151a88c0b64e772295461f10e9
leonpatrushev <leonpatrushev@yandex.ru>
2021-06-19 02:52:03 +0500

    REAL DELIVER VERSION

--- LibraryWpfApp/Models/LibraryDataBaseModel.Designer.cs
+++ LibraryWpfApp/Models/LibraryDataBaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\leonp\Desktop\Курсовая\LibraryWpfApp\LibraryWpfApp\Models\LibraryDataBaseModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\leonp\Desktop\LibraryWpfApp\LibraryWpfApp\Models\LibraryDataBaseModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
